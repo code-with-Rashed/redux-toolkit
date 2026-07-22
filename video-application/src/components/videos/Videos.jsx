669caca2ef86a24da1207d40ce67ff418e4e674a
Rashed <rashedalamj@gmail.com>
2026-07-22 18:25:38 +0600

    feat: implement video fetching and loading states with Redux integration

--- video-application/src/components/videos/Videos.jsx
+++ video-application/src/components/videos/Videos.jsx
@@ -1,13 +1,28 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
+import Error from "../ui/Error";
+import VideoLoader from "../ui/loaders/VideoLoader";
+import Success from "../ui/Success";
 import Video from "./Video";
 
 const Videos = () => {
-  return (
-    <>
-      <Video />
-      <Video />
-      <Video />
-      <Video />
-    </>
-  );
+  const { data: videos, isLoading, isError, error } = useGetVideosQuery();
+
+  let content = null;
+  if (isLoading) {
+    content = Array.from({ length: 4 }, (_, index) => index).map((loader) => (
+      <VideoLoader key={loader} />
+    ));
+  }
+  if (!isLoading && isError) {
+    content = <Error message={error} />;
+  }
+  if (!isLoading && !isError && videos?.length === 0) {
+    content = <Success message="Video Not Found." />;
+  }
+  if (!isLoading && !isError && videos?.length > 0) {
+    content = videos.map((video) => <Video key={video.id} video={video} />);
+  }
+
+  return content;
 };
 export default Videos;
